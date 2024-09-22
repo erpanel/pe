19d70a94f1329bebe414a2491cf10b7c30d4ab0c
official4jelas <ydiskyha@gmail.com>
2024-09-23 01:58:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ global.prems = ['6285766450735']
 global.nameowner = 'botzz'
 global.numberowner = '6285766450735' 
 global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
+global.gc = 'https://chat.whatsapp.com'
 global.instagram = 'https://instagram.com/prm2.0'
 global.wm = '© botzz'
 global.wait = '_*Tunggu sedang di proses...*_'
@@ -13,21 +13,22 @@ global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made With'
 global.author = 'Bot WhatsApp'
 global.autobio = false // Set true untuk mengaktifkan autobio
-global.maxwarn = '1' // Peringatan maksimum
+global.maxwarn = '5' // Peringatan maksimum
 
 //INI WAJIB DI ISI!//
-global.btc = 'botzz' 
+global.btc = '4zoe6D2Z' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.lann = 'bottz'
+global.lann = 'YOUR_APIKEY_HERE'
 //Daftar https://api.betabotz.eu.org 
 
 global.APIs = {   
-  btc: 'https://api.botcahx.eu.org'
+  btc: 'https://api.botcahx.eu.org',
+  widipe: 'https://widipe.com'
 }
 global.APIKeys = { 
-  'https://api.botcahx.eu.org': 'botzz' 
+  'https://api.botcahx.eu.org': '4zoe6D2Z' 
 }
 
 let fs = require('fs')
